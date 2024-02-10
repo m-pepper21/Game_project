ceddcf8a6bf8962e563e39eef0c181cfe23735dc
Margarida Pimenta <margarida_p21@hotmail.com>
2024-02-10 18:21:56 +0000

    new changes

--- src/enemy.js
+++ src/enemy.js
@@ -1,3 +1,5 @@
+import MovingDirection from "./movingdirection.js";
+
 export default class Enemy {
   constructor(x, y, tileSize, velocity, tileMap) {
     this.x = x;
@@ -7,17 +9,83 @@ export default class Enemy {
     this.tileMap = tileMap;
 
     this.#loadImages();
+
+    this.movingDirection = Math.floor(
+      Math.random() * Object.keys(MovingDirection).length
+    );
+
+    this.directionTimerDefault = this.#random(10, 50);
   }
 
   draw(ctx) {
+    this.#move();
+    this.#changeDirection();
     ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
   }
+
+  #changeDirection() {
+    this.directionTimer--;
+    let newMoveDirection = null;
+    if (this.directionTimer == 0) {
+      this.directionTimer = this.directionTimerDefault;
+      newMoveDirection = Math.floor(
+        Math.random() * Object.keys(MovingDirection).length
+      );
+    }
+
+    if (newMoveDirection != null && this.movingDirection != newMoveDirection) {
+      if (
+        Number.isInteger(this.x / this.tileSize) &&
+        Number.isInteger(this.y / this.tileSize)
+      ) {
+        if (
+          !this.tileMap.didCollideWithEnvironment(
+            this.x,
+            this.y,
+            newMoveDirection
+          )
+        ) {
+          this.movingDirection = newMoveDirection;
+        }
+      }
+    }
+  }
+
+  #move() {
+    if (
+      !this.tileMap.didCollideWithEnvironment(
+        this.x,
+        this.y,
+        this.movingDirection
+      )
+    ) {
+      switch (this.movingDirection) {
+        case MovingDirection.up:
+          this.y -= this.velocity;
+          break;
+        case MovingDirection.down:
+          this.y += this.velocity;
+          break;
+        case MovingDirection.left:
+          this.x -= this.velocity;
+          break;
+        case MovingDirection.right:
+          this.x += this.velocity;
+          break;
+      }
+    }
+  }
+
+  #random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   #loadImages() {
     this.normalBroccoli = new Image();
-    this.normalBroccoli.src = "..images/images/broccoli.jpg";
+    this.normalBroccoli.src = "../images/images/broccoli.jpg";
 
     this.scaredBroccoli = new Image();
-    this.scaredBroccoli.src = "..images/images/scared_broccoli.jpg";
+    this.scaredBroccoli.src = "../images/images/scared_broccoli.jpg";
 
     this.scaredBroccoli2 = new Image();
     this.scaredBroccoli2.src = "../images/images/scared_broccoli2.jpg";
