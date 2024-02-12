7911799067e91cfac653ad24a14464d97cc8b942
Margarida Pimenta <margarida_p21@hotmail.com>
2024-02-12 17:04:14 +0000

    //final commit

--- src/game.js
+++ src/game.js
@@ -16,9 +16,20 @@ const gameWinSound = new Audio("../sounds/gameWin.wav");
 
 function gameLoop() {
   tileMap.draw(ctx);
+  drawGameEnd();
   Trinny.draw(ctx, pause(), enemies);
   enemies.forEach((enemy) => enemy.draw(ctx, pause(), Trinny));
   checkGameOver();
+  checkGameWin();
+}
+
+function checkGameWin() {
+  if (!gameWin) {
+    gameWin = tileMap.didWin();
+    if (gameWin) {
+      gameWinSound.play();
+    }
+  }
 }
 
 function checkGameOver() {
@@ -37,7 +48,28 @@ function isGameOver() {
 }
 
 function pause() {
-  return !Trinny.madeFirstMove || gameOver;
+  return !Trinny.madeFirstMove || gameOver || gameWin;
+}
+
+function drawGameEnd() {
+  if (gameOver || gameWin) {
+    let text = "You Win!";
+    if (gameOver) {
+      text = "You Died!";
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, canvas.height / 3.2, canvas.width, 80);
+
+    ctx.font = "80px comic sans";
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop("0", "magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+
+    ctx.fillStyle = gradient;
+    ctx.fillText(text, 37, canvas.height / 2);
+  }
 }
 
 tileMap.setCanvasSize(canvas);
